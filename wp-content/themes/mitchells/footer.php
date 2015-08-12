178702d4618612c7428e1d0a7815972178b7fd81
sheeksav <andy.sheeks@gmail.com>
2015-08-11 20:31:26 -0400

    Modifying some things in the footer.

--- wp-content/themes/mitchells/footer.php
+++ wp-content/themes/mitchells/footer.php
@@ -1,35 +1,23 @@
 <div class="footer">
-<div class="container">
-<div class="row">
-  <div class="col-xs-5 col-xs-offset-1">
-	<iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=569997107/size=small/bgcol=ffffff/linkcol=e99708/artwork=none/transparent=true/" seamless><a href="http://themitchells.bandcamp.com/album/the-mitchells">The Mitchells by The Mitchells</a></iframe>
-  </div>
-
-  <div class="col-xs-1">
-    <a href="https://www.facebook.com/TheMitchellsMusic" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-facebook.png" /></a>
-  </div>
-
-  <div class="col-xs-1">
-    <a href="https://twitter.com/themitchells" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-twitter.png" /></a>
-  </div>
-
-  <div class="col-xs-1">
-    <a href="https://itunes.apple.com/us/album/the-mitchells/id924089045" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-itunes.png" /></a>
-  </div>
-
-  <div class="col-xs-1">
-    <a href="https://play.spotify.com/album/2PSST1ap4ynOFL9dp5cYjW" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-spotify.png" /></a>
-  </div>
-
-  <div class="col-xs-1">
-    <a href="https://soundcloud.com/themitchells-2" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-soundcloud.png" /></a>
-  </div>
+    <div class="container">
+        <div class="row">
 
 
+            <div class="col-xs-12 col-md-6" id="footer-left">
+                <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=569997107/size=small/bgcol=ffffff/linkcol=e99708/artwork=none/transparent=true/" seamless><a href="http://themitchells.bandcamp.com/album/the-mitchells">The Mitchells by The Mitchells</a></iframe>
+            </div> <!--/.col-->
 
-</div>
-</div>
-</div>
+            <div class="col-xs-12 col-md-6" id="footer-right">
+                <a class="social-icon" href="https://www.facebook.com/TheMitchellsMusic" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-facebook.png" /></a>
+                <a class="social-icon" href="https://twitter.com/themitchells" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-twitter.png" /></a>
+                <a class="social-icon" href="https://itunes.apple.com/us/album/the-mitchells/id924089045" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-itunes.png" /></a>
+                <a class="social-icon" href="https://play.spotify.com/album/2PSST1ap4ynOFL9dp5cYjW" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-spotify.png" /></a>
+                <a class="social-icon" href="https://soundcloud.com/themitchells-2" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/mitchells-soundcloud.png" /></a>
+            </div> <!--/.col-->
+
+        </div> <!--/.row-->
+    </div> <!--/.container-->
+</div> <!--/.footer-->
 
 
 <div class="footer-bottom">
