4252bedc31d6cbf83c1d123db62f2e79418d175f
sheeksav <andy.sheeks@gmail.com>
2015-08-11 21:56:20 -0400

    uhhhh

--- wp-content/themes/mitchells/footer.php
+++ wp-content/themes/mitchells/footer.php
@@ -1,12 +1,4 @@
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
-    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="<?php bloginfo('template_url'); ?>/js/ie10-viewport-bug-workaround.js"></script>
 
     <?php wp_footer(); ?>
 
