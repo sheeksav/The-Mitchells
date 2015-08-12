3efe2f16d2e6447e0cfda62dd05e35d72601bbd3
sheeksav <andy.sheeks@gmail.com>
2015-08-11 21:46:17 -0400

    localized the jquery file

--- wp-content/themes/mitchells/footer.php
+++ wp-content/themes/mitchells/footer.php
@@ -2,8 +2,9 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="<?php bloginfo('template_url'); ?>/js/ie10-viewport-bug-workaround.js"></script>
 
